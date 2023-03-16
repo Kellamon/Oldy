c7280b80bbe6542b47905de0da9a48cca90c5d77
Kellamon <Badzed@list.ru>
2023-03-16 17:12:05 +0300

    Class Work 4 added

---
+++ CW4/src/main/java/org/example/cw1/Ex1.java
@@ -0,0 +1,13 @@
+package org.example.cw1;
+
+
+import java.util.LinkedList;
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+    }
+
+}
